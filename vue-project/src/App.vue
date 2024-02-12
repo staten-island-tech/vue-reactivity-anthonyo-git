11a95317eba2911a74619d8152d24aa3cf8ee26e
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2024-02-11 21:50:02 -0500

    AD CAP

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,47 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const money = ref(0);
+
+setInterval(() => {
+  money.value +=  321315325386858678769766796796967967932525; 
+},  1)
 </script>
 
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="game-container">
+    <div class="money-display">${{ money }}</div>
+    <div class="rectangle" v-for="x in  10" :key="x">
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+.money-display {
+  position: absolute;
+  top: 10px; 
+  left: 10px; 
+  font-size: 35px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.game-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+
+.rectangle {
+  flex-basis: calc(50% - 100px);;
+  width:  1200px; 
+  height:  125px; 
+  background-color: #eee; 
+  border:  1px solid #000;
+  margin:  10px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
