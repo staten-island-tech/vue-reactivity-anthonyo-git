cdcb93b112dcd1988d3c8592d8063f83db1d817a
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2024-02-25 21:10:19 -0500

    add ach over + cover

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,82 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
+
+const showOverlay = ref(false);
 </script>
 
 <template>
-  <header>
-
+  <div class="body">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
       <nav>
-        <RouterLink to="/">Earth</RouterLink>
-        <RouterLink to="/about">achievement</RouterLink>
       </nav>
     </div>
-  </header>
-
   <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style scoped></style>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- vue-project/src/components/earthWorld.vue
+++ vue-project/src/components/earthWorld.vue
@@ -2,7 +2,7 @@
   <div class="body">    
     <div class="Heading-Bar">
       <div class="Captalist">
-        <img src="https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/capitalist.png" alt="captalistMan">
+        <img src="https://i.imgur.com/fFc9Jsg.png" alt="captalistMan">
       </div>
       <div class="money-display">${{ money }}</div>
     </div>
@@ -37,120 +37,75 @@ const addMoney = (amount) => {
 //   money.value +=  1; 
 // },  1000)
 
-const rectangles = ref([
-  { name: 'Lemonade Stand  ' },
-  { name: 'Newspaper Delivery  ' },
-  { name: 'Car Wash  ' },
-  { name: 'Pizza Delivery  ' },
-  { name: 'Donut Shop  ' },
-  { name: 'Shrimp Boat  ' },
-  { name: 'Hockey Team  ' },
-  { name: 'Movie Studio  ' },
-  { name: 'Bank  ' },
-  { name: 'Oil Company  ' },
-]);
-
-
-
 const businesses = ref([
   {
-    id: 'lemonadeStand',
-    rectangleNum:  1,
     name: 'Lemonade stand',
     price:  1,
-    timeTaken:  1,
     quantityPurchased:  1,
     profit:  1,
     image: 'https://i.imgur.com/uf6dPpY.png',
   },
   {
-    id: 'newspaperDelivery',
-    rectangleNum:  2,
-    name: 'Newspaper Deliver',
-    price:  30,
-    timeTaken:  3,
-    quantityPurchased:  0,
-    profit:  25,
-    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/newspaper.png',
-  },
-  {
-    id: 'carWash',
-    rectangleNum:  3,
-    name: 'Car Wash',
-    price:  500,
-    timeTaken:  6,
-    quantityPurchased:  0,
-    profit:  300,
-    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/car.png',
-  },
-  {
-    id: 'pizzaDelivery',
-    rectangleNum:  4,
-    name: 'Pizza Delivery',
-    price:  3000,
-    timeTaken:  12,
-    quantityPurchased:  0,
-    profit:  2000,
-    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/pizza.png',
-  },
-  {
-    id: 'donutShop',
-    rectangleNum:  5,
-    name: 'Donut Shop',
-    price:  50000,
-    timeTaken:  30,
-    quantityPurchased:  0,
-    profit:  20000,
-    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/donut.png',
-  },
-  {
-    id: 'shrimpBoat',
-    rectangleNum:  6,
     name: 'Shrimp Boat',
     price:  800000,
-    timeTaken:  90,
     quantityPurchased:  0,
-    profit:  4000000,
+    profit: 622080,
     image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/boat.png',
   },
   {
-    id: 'hockeyTeam',
-    rectangleNum:  7,
+    name: 'Newspaper Deliver',
+    price:  30,
+    quantityPurchased:  0,
+    profit:  60,
+    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/newspaper.png',
+  },
+  {
     name: 'Hockey Team',
     price:  6000000,
-    timeTaken:  600,
     quantityPurchased:  0,
-    profit:  4000000,
+    profit:  7464960,
     image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/hockey.png',
   },
   {
-    id: 'movieStudio',
-    rectangleNum:  8,
+    name: 'Car Wash',
+    price:  500,
+    quantityPurchased:  0,
+    profit:  540,
+    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/car.png',
+  }, 
+  {
     name: 'Movie Studio',
     price:  100000000,
-    timeTaken:  3600,
     quantityPurchased:  0,
-    profit:  40000000,
+    profit:  89579520,
     image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/movie.png',
   },
   {
-    id: 'bank',
-    rectangleNum:  9,
+    name: 'Pizza Delivery',
+    price:  3000,
+    quantityPurchased:  0,
+    profit:  4320,
+    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/pizza.png',
+  }, 
+  {
     name: 'Bank',
     price:  5000000000,
-    timeTaken:  36000,
     quantityPurchased:  0,
-    profit:  1000000000,
+    profit:  1074954520,
     image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/bank.png',
   },
   {
-    id: 'oilCompany',
-    order:  10,
+    name: 'Donut Shop',
+    price:  50000,
+    quantityPurchased:  0,
+    profit:  51840,
+    image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/donut.png',
+  },
+  {
     name: 'Oil Company',
     price:  153434564431,
-    timeTaken:  36000,
     quantityPurchased:  0,
-    profit:  51434564431,
+    profit:  29668737024,
     image: 'https://10288944-884245489883600444.preview.editmysite.com/uploads/b/4181373-341607469518366532/files/images/company.png',
   },
 ]);
@@ -161,33 +116,33 @@ const businesses = ref([
 
 .body {
   font-family: "Comic Sans",cursive;
-  font-weight: bold;
-  font-size:   18px;
+  font-size: 18px;
   color: #dedede;
-  font-weight:   700;
-  background-color: #242121a8;
+  font-weight: 700;
+  background-color: #141414;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
 
 .imageBusinesses {
-  height:   64px;
-  width:   64px;
+  height: 64px;
+  width: 64px;
 }
 
 .Heading-Bar {
   background-color: #473f3fb6;   
-  padding:   20px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .Captalist img {
-  width:   10%;
+  width: 13vw;
 }
 
 .money-display {
-  font-size:   24px;
+  font-size: 44px;
   font-weight: bold;
   color:white;
 }
@@ -198,26 +153,25 @@ const businesses = ref([
   flex-wrap: wrap;
   justify-content: center; 
   align-items: center; 
+  padding: 20px;
+  flex-grow: 1;
 }
 
 .business-item {
   display: flex;
   align-items: center;
-  margin:   10px;
+  margin: 10px;
 }
 
 .rectangle {
-  width:   1200px;
-  height:   125px;
-  border:   1px solid #50474700;
-  cursor: pointer;
-  background-color: #8c8176;
-  border-radius:   10px;
+  width: 35vw;
+  height: 11vh;
+  background-color: #425270;
+  border-radius: 10px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-left:   10px; 
+  margin-left: 10px; 
 }
 
 </style>
@@ -226,3 +180,4 @@ const businesses = ref([
 // https://vuejs.org/guide/essentials/list
 // https://adventure-capitalist.fandom.com/wiki/Businesses
 // css cover x until money balance is reaches a certain amount
+// https://gaming.stackexchange.com/questions/330133/what-s-the-price-scaling-for-businesses
